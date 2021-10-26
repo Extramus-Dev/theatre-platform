45d01a46fe63d9f50ee80fc522124d0ab32dd241
renanaferreira <renanaferreira1999@gmail.com>
2021-10-26 16:39:22 +0200

    finals changes for change scenario in app

---
+++ entry-point.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export DB_HOST=theater-platform-db
+export DB_PORT=3306
+export API_HOST=theater-platform-django-api-server
+export API_PORT=8000
+export UI_HOST=theater-platform-web-ui
+export UI_PORT=8080
+
+export DB_NAME=theater-platform
+export DB_USER='theater_admin'
+export DB_USER_PW='theater_platform2021'
+export DB_ROOT_PW=root
+docker-compose build
+docker-compose up
