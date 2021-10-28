d67d99062ee042ca73484a08ced500860ed167af
renanaferreira <renanaferreira1999@gmail.com>
2021-10-28 12:47:45 +0200

    First user story for viewer signup in API

--- backend/apps/user_management/views.py
+++ backend/apps/user_management/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+from .serializers import ViewerSerializer
+from .models import ViewerProfile
+
+
+class SignUPViewer(APIView):
+    """
+    Sign Up viewer Rest Api format
+    """
+
+    def post(self, request, format=None):
+        serializer = ViewerSerializer(data=request.DATA)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
